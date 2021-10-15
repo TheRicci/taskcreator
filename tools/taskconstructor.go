b62e7bcf6b3a5d52958f9b9aa8fa8ea7ac48e08e
0xSirius <41972780+TheRicci@users.noreply.github.com>
2021-10-15 00:05:51 -0300

    1

---
+++ tools/taskconstructor.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type Task struct {
+	timer  time.Duration
+	ticker time.Ticker
+	quit   chan int
+}
+
+func NewTask(initialDelay time.Duration, tick time.Duration) Task {
+	return Task{
+		timer:  tick,
+		ticker: *time.NewTicker(initialDelay),
+		quit:   make(chan int),
+	}
+}
+
+func (t Task) Start(task func(), runAsync bool) {
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		defer func() {
+			t.close()
+			fmt.Println("Scheduler stopped!!")
+		}()
+		firstExec := true
+		for {
+			select {
+			case <-t.ticker.C:
+
+				if firstExec {
+					t.ticker.Stop()
+					t.ticker = *time.NewTicker(t.timer)
+					firstExec = false
+				}
+
+				if runAsync {
+					go task()
+				} else {
+					task()
+				}
+
+				break
+			case <-t.quit:
+				return
+			case <-sigs:
+				_ = t.Close()
+				break
+			}
+		}
+
+	}()
+
+}
+
+func (t *Task) Close() error {
+	go func() {
+		t.quit <- 1
+	}()
+	return nil
+}
+func (t *Task) close() {
+	t.ticker.Stop()
+}
+
+func timeStampMillis() int {
+	return int(time.Now().UnixNano() / 1000000)
+}
+
+func main() {
+
+	totalCount := 0
+
+	go NewTask(10*time.Second, 2*time.Second).Start(func() {
+		totalCount++
+		fmt.Printf("%d.%4stime is %d\n", totalCount, " ", timeStampMillis())
+	}, true)
+
+	time.Sleep(time.Minute)
+
+}
